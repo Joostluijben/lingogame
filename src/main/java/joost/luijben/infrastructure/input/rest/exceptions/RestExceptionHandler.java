58fc9ac04af70dd0c880b51d78cd0b067ae6db0d
Joost Luijben <joost.luijben@student.hu.nl>
2020-06-14 23:57:44 +0200

    Hotfix duplications of playerName

--- src/main/java/joost/luijben/infrastructure/input/rest/exceptions/RestExceptionHandler.java
+++ src/main/java/joost/luijben/infrastructure/input/rest/exceptions/RestExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
+import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -16,6 +17,12 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorModel error = new ErrorModel(HttpStatus.NOT_FOUND, "Entity not found", ex.getMessage());
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    private ResponseEntity<ErrorModel> handleEntityExistsException(ConstraintViolationException ex) {
+        ErrorModel error = new ErrorModel(HttpStatus.CONFLICT, "Entity already exists", ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
     }
 }
 
